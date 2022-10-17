44f64eb3733138daf8cad5073124120d3e80ce89
Reueben Chavez <rchavez0215@gmail.com>
2022-10-16 23:39:06 -0700

    Adding cinemachine

---
+++ Homework_1/Assets/ChangeCameraButton.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ChangeCameraButton : MonoBehaviour
+{
+    public List<Camera> Cameras;
+    private VisualElement frame;
+    private Button button;
+
+    private void OnEnable()
+    {
+        var uiDocument = GetComponent<UIDocument>();
+        var rootVisualElement = uiDocument.rootVisualElement;
+        frame = rootVisualElement.Q<VisualElement>("Frame");
+        button = frame.Q<Button>("Button");
+        button.RegisterCallback<ClickEvent>(ev => ChangeCamera());
+    }
+
+    int click = 0;
+    private void ChangeCamera(){
+        EnableCamera(click);
+        click++;
+        if (click > 3) {
+            click = 0;
+        }
+    }
+
+    private void EnableCamera(int n) {
+        Cameras.ForEach(cam => cam.enabled = false);
+        Cameras[n].enabled = true;
+    }
+}
